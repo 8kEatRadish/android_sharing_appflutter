6a15c45ad5200c5327b349c3cc13e5d842b28bcb
sniperking <18845795735@163.com>
2019-05-05 23:50:58 +0800

    feat: 完成flutter与原生传递数据（电池电量）

--- sharing_app/android/app/src/main/java/com/example/sharing_app/MainActivity.java
+++ sharing_app/android/app/src/main/java/com/example/sharing_app/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.sharing_app;
 
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
+
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "samples.flutter.io/battery";
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    new MethodChannel(getFlutterView(),CHANNEL).setMethodCallHandler(
+            (methodCall, result) -> {
+              if(methodCall.method.equals("getBatteryLevel")){
+                int betteryLevel = getBatteryLevel();
+                if (betteryLevel != -1){
+                  result.success(betteryLevel);
+                }else {
+                  result.error("UNAVAILABLE", "Battery level not available.", null);
+                }
+              }else{
+                result.notImplemented();
+              }
+            }
+    );
+  }
+  /**
+   * 获取电池电量
+   *
+   * @return
+   */
+  private int getBatteryLevel() {
+
+    int batteryLevel = -1;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+    } else {
+      Intent intent = new ContextWrapper(getApplicationContext()).
+              registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+      batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+              intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+    }
+    return batteryLevel;
   }
 }
